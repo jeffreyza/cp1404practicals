d4a4db529985f8c74a6345bfb0af425e287e4e81
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-07-30 11:33:26 +0800

    Created random score generator to test score check function.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,10 +2,15 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
+
+
 def main():
     score = float(input("enter score: "))
     check_score(score)
-    print (check_score)
+    print(check_score(score))
+    random_score = random.randint(0, 100)
+    print("Your random score is", random_score, "and it is a", check_score(random_score))
 
 
 def check_score(score):
