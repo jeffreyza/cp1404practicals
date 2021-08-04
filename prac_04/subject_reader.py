c3f7052c0404a3f626f379bbaf1371a8e60362ac
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-08-04 19:13:42 +0800

    Added function to format data correctly and to be easy to read. Appended list inside a new list.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,12 +8,13 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    subjects_formatter(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    new_data_list = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,7 +24,14 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        new_data_list.append(parts)
     input_file.close()
+    return new_data_list
+
+
+def subjects_formatter(data):
+    for subject in data:
+        print("{:1} Is taught by {:2} and has {:3} students".format(*subject))
 
 
 main()
