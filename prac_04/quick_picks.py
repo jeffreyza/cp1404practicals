55bda0b5ecf70da9bec8d51b97639846e95d0f4e
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-08-06 11:49:21 +0800

    Added random generator to generate 6 numbers and append to list. repeat based on input.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,8 +1,20 @@
 import random
 
-MAX_NUMBERS = 6
+MAX_NUMBER = 6
+MIN_PICK = 1
+MAX_PICK = 45
 
 
 def main():
+    picks_number = int(input("How many quick picks do you want? "))
+
+    for num in range(picks_number):
+        quick_picks_list = []
+        for i in range(MAX_NUMBER):
+            numbers = random.randint(MIN_PICK, MAX_PICK)
+            quick_picks_list.append(numbers)
+        quick_picks_list.sort()
+        print(*quick_picks_list)
+
 
 main()
