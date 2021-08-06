adcbf388ca37405ae06e0c2eb9adc0126b068f86
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-08-06 12:16:22 +0800

    Added constants.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,13 +7,12 @@ MAX_PICK = 45
 
 def main():
     picks_number = int(input("How many quick picks do you want? "))
-
     for num in range(picks_number):
         quick_picks_list = []
         for i in range(MAX_NUMBER):
             numbers = random.randint(MIN_PICK, MAX_PICK)
             while numbers in quick_picks_list:
-                numbers = random.randint(1, 45)
+                numbers = random.randint(MIN_PICK, MAX_PICK)
             quick_picks_list.append(numbers)
         quick_picks_list.sort()
         print(*quick_picks_list)
