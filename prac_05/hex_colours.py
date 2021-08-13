b34e9c84e1f560089e9adefd31317eafe210b202
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-08-13 10:40:50 +0800

    Add color hexadecimal dictionary

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,3 +3,7 @@ CP1404/CP5632 Practical
 Jeffrey Timms
 Hexadecimal colour picker
 """
+
+COLOR_HEX = {"BlanchedAlmond": "#ffebcd", "Brown": "#a52a2a", "CadetBlue": "#5f9ea0", "Coral": "#ff7f50",
+             "CornflowerBlue": "#6495ed", "DarkGreen": "#006400", "DarkOrange": "#ff8c00", "DarkViolet": "#9400d3",
+             "GhostWhite": "#f8f8ff", "Goldenrod": "#daa520"}
