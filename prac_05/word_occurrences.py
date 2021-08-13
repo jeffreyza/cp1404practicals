cda0a7c6fff66ff6321a4e189cfbca31548e0c71
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-08-13 11:40:47 +0800

    split input and add to dictionary

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,3 +3,13 @@ CP1404/CP5632 Practical
 Jeffrey Timms
 Count occurrences of words in a string
 """
+
+words_dictionary = {}
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+for word in words:
+    try:
+        words_dictionary[word] += 1
+    except KeyError:
+        words_dictionary[word] = 1
+
