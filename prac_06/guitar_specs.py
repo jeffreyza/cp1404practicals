aa72111b727b09fbe72f1feb8e7bd7462ce71726
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-08-27 11:34:25 +0800

    Return string of guitarspecs

--- prac_06/guitar_specs.py
+++ prac_06/guitar_specs.py
@@ -1,3 +1,12 @@
 
 class GuitarSpecs:
     def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.year}) : ${self.cost:2f}"
+
+
+
