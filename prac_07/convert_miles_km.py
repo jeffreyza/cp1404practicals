d1c6d6056353f88c93564d2a23c1ecc75aaf2496
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-09-06 22:58:32 +0800

    Add docstrings to describe functions

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -10,12 +10,14 @@ class ConvertMilesToKM(App):
     km_output = StringProperty()
 
     def build(self):
+        """Build app in window 500x200"""
         self.title = "Convert Miles to KM"
         Window.size = (500, 200)
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
 
     def validate_number(self):
+        """Validates and returns 0.0 if not a valid number"""
         if self.root.ids.miles_input.text == '':
             return 0.0
         else:
@@ -26,10 +28,12 @@ class ConvertMilesToKM(App):
                 return 0.0
 
     def handle_increment(self, number):
+        """increment the user input number by 1 or -1 based on button selection"""
         number_to_increment = self.validate_number()
         self.root.ids.miles_input.text = str(number_to_increment + number)
 
     def km_conversion(self):
+        """Convert input miles to KM based on 1 mile = 1.60934"""
         miles = self.validate_number()
         kilometers = miles * MILES_IN_KM
         self.root.ids.km_output.text = str(kilometers)
