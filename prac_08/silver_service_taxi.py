98022ba086b38aecebb58a46fb65adf178d07c51
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-09-10 11:09:12 +0800

    Initial Commit

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -1,6 +1,6 @@
 """
 CP1404/CP5632 Practical
-SilverServiceTaxi
+SilverServiceTaxi Class
 Jeffrey Timms
 """
 
---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,6 @@
+"""
+CP1404/CP5632 Practical
+Silver Service Taxi test
+Jeffrey Timms
+"""
+
