a9e8d2d1014937b410bb9e5badfc26f869cd61e9
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-09-10 11:07:08 +0800

    Add overridden str to display flagfall inclusive

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -5,3 +5,19 @@ Jeffrey Timms
 """
 
 from prac_08.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    
+    flagfall = 4.5
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = self.price_per_km * fanciness
+
+    def __str__(self):
+        return f"{super().__str__()} Including flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        return self.price_per_km + self.flagfall
