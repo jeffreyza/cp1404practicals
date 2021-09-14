c29b0b2073d98e935e8923f9d9ec72b19cdf22db
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-09-14 23:06:29 +0800

    Add basic menu to main()

---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,14 @@
+from silver_service_taxi import SilverServiceTaxi
+from taxi import Taxi
+
+def main():
+    current_taxi = None
+    print("Let's drive!")
+    print("q)uit, c)hoose taxi, d)drive")
+    choice = input("Enter choice: ").lower()
+    while choice != "q":
+        if choice == "c":
+            #get taxi method
+        if choice == "d":
+            #get drive method
+
