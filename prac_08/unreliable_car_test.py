5f94ae4880fb730b94a0eecf31c6aa6b059020de
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-09-14 17:35:55 +0800

    Add car to test unreliable_car

---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,7 @@
+from prac_08.unreliable_car import UnreliableCar
+
+my_car = UnreliableCar(name="Lada Vesta 2018", fuel=150, reliability=20)
+
+my_car.drive(50)
+print(f"{my_car.name}, attempted to drive 50 miles and managed to drive {my_car.odometer} Mi, there is {my_car.fuel} "
+      f"fuel remaining")
