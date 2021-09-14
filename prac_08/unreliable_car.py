6120fa8241f717f3f51d2a4e228b8056af071a49
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-09-14 17:26:25 +0800

    Add drive() to drive distance if reliability is > random_reliability

---
+++ prac_08/unreliable_car.py
@@ -0,0 +1,22 @@
+from prac_08.taxi import Car
+import random
+
+
+class UnreliableCar(Car):
+
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        random_reliability = random.randint(0, 100)
+        if self.reliability > random_reliability:
+            distance = super().drive(distance)
+        else:
+            distance = 0
+
+        return distance
+
+
+
+
