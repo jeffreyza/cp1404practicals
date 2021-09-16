20e16c32682fb9dd232f4c943da0c0e7b5ad2bf0
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-09-16 23:03:41 +0800

    Add display_taxis to display list of taxis available

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -1,16 +1,25 @@
 from silver_service_taxi import SilverServiceTaxi
 from taxi import Taxi
+from car import Car
 
 def main():
+    taxis = [Taxi("Honda Accord", 140), SilverServiceTaxi("Toyota Prius", 200, 3), SilverServiceTaxi("Hummer H1", 300, 5)]
     current_taxi = None
     print("Let's drive!")
     print("q)uit, c)hoose taxi, d)drive")
     choice = input("Enter choice: ").lower()
     while choice != "q":
         if choice == "c":
-            #get taxi method
+            print("Taxi's Available:")
+            display_taxis()
         if choice == "d":
-            #get drive method
+
+
         else:
             print("Invalid menu choice")
         choice = input("Enter choice: ").lower()
+
+
+def display_taxis(taxis)
+    for i, taxi in enumerate(taxis):
+        print(f"{i}, {taxi}")
