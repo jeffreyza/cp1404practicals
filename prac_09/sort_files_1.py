95c6adae63a4274252429c40a40192ad5cf81b30
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-09-20 15:05:29 +0800

    Bug fix, only make directory if extension is not empty

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -5,6 +5,9 @@ import pathlib
 os.chdir('FilesToSort')
 for filename in os.listdir('.'):
     #    print(pathlib.Path(filename).suffix)
-    extension = os.path.splitext(filename)[1][1:]
-    if not os.path.exists(extension):
-        os.mkdir(extension)
+    extension = os.path.splitext(filename)[1][1:]  # will extract text after . to get extension only
+    if not os.path.exists(extension):  # if directory doesn't exist create it
+        if extension != "":
+            os.mkdir(extension)
+
+extensions = ["doc", "docx", "gif", "jpg", "png", "txt", "xls", "xlsx"]
