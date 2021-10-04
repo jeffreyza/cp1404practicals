cae4232db3a2965425d136517a31433905d55799
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-10-04 22:59:13 +0800

    complete testing for phrase format

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -38,7 +38,6 @@ def run_tests():
     test_car = Car()
     assert test_car.odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
@@ -56,12 +55,17 @@ doctest.testmod()
 
 # TODO: 5. Write and test a function to format a phrase as a sentence,
 def phrase_format(phrase):
-
-# starting with a capital and ending with a single full stop.
-# Important: start with a function header and just use pass as the body
-# then add doctests for 3 tests:
-# 'hello' -> 'Hello.'
-# 'It is an ex parrot.' -> 'It is an ex parrot.'
-# and one more you decide (one that is valid!)
-# test this and watch the tests fail
-# then write the body of the function so that the tests pass
+    """    Determine if function is working correctly.
+    >>> phrase_format("hello")
+    'Hello.'
+    >>> phrase_format('It is an ex parrot.')
+    'It is an ex parrot.'
+    >>> phrase_format('ethereum to the moon')
+    'Ethereum to the moon.'
+    """
+    sentence = phrase.capitalize()
+    if sentence[-1] == ".":
+        return sentence
+    else:
+        sentence += "."
+        return sentence
