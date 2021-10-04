722d7a62bc17e421c5906146e8f5d0ae0d471682
Jeffrey <jeffrey.timms@my.jcu.edu.au>
2021-10-04 23:37:13 +0800

    Print URL and Title of wiki page

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -4,8 +4,9 @@ user_choice = input("Enter a page title or phrase to search: ")
 
 while user_choice != "":
     try:
-        page_selected = wikipedia.page(user_choice, auto_suggest=False)
+        page_selected = wikipedia.page(user_choice)
         print(f"Title: {page_selected.title}")
+        print(f"Url: {page_selected.url}")
         print(f"Summary: {page_selected.summary}")
         user_choice = input("Enter a page title or phrase to search: ")
     except wikipedia.DisambiguationError as e:
